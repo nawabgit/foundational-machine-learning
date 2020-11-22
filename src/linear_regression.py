75db037984275e1f455441c2982cfe98f1ac8952
Nawab <nawab.mir14@gmail.com>
2020-11-22 01:06:03 +0000

    added random training data with noise

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -9,6 +9,25 @@ def four_pi_sin(xin):
     return anp.array(anp.sin(4 * np.pi * xin))
 
 
-X_plot = np.linspace(-1,1,200)
-plt.plot(X_plot, four_pi_sin(X_plot))
-plt.show()
+def run_linear_regression(n=15):
+    """ Executes Linear Regression
+
+    :param n: The number of (x_n, t_n) pairs in the training set
+
+    """
+    # Set random seed for sanity
+    np.random.seed(11)
+
+    # Generate n random points from 0 - 1
+    rn = np.random.uniform(0, 1, n)
+    x_training = np.reshape(np.sort(rn, axis=0), (n, 1))
+
+    # Calculate y values and add noise
+    noise = np.reshape(np.random.normal(0, 0.1, n), (n, 1))
+    y_training = four_pi_sin(x_training) + noise
+
+    plt.plot(x_training, y_training)
+    plt.show()
+
+
+run_linear_regression()
